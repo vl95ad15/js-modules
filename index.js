6654a025b8398945b5bc2dae618678a45ed610db
Vlad <martsul.vlad@gmail.com>
2020-10-13 23:07:37 +0300

    Fixed modules

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import { checkType, nonEmptyString, numberInRange } from "./modules-package/check.js";
-import { validateFiled, validateObject } from "./modules-package/validation.js";
+import { validateObject } from "./modules-package/validation.js";
 import { dumpErrors } from "./modules-package/errors.js";
 import { user } from "./modules-package/scheme.js";
 import { userScheme } from "./modules-package/scheme.js";
